bcd5d1a1c22e638a9dcad3f71059c5ea7c3df080
saifullah <maliksaifullah304@gmail.com>
2025-04-26 21:46:09 +0500

    remove unused imports

--- components/offerPrayerImg.js
+++ components/offerPrayerImg.js
@@ -1,15 +1,15 @@
-import { Box, dividerClasses, Grid, Stack, Typography } from "@mui/material";
-import Image from "next/image";
+import {Stack} from '@mui/material';
+import Image from 'next/image';
 
 export default function OfferPrayerImg() {
   return (
     <Stack
-      sx={{ display: { xs: "none", md: "flex" } }}
-      alignItems={"center"}
-      width={"50%"}
+      sx={{display: {xs: 'none', md: 'flex'}}}
+      alignItems={'center'}
+      width={'50%'}
       pb={9}
     >
-      <Image alt="" width={600} height={700} src={"/Prayer3.jpg"} />
+      <Image alt="" width={600} height={700} src={'/Prayer3.jpg'} />
     </Stack>
   );
 }
